03deb778f745e7d6a3fbf2b671820a38b361027d
AleksandrKotliar <sashagsmnew@gmail.com>
2022-11-30 19:37:20 +0400

    Configurating Photo Collection View add extension

---
+++ MyPhotoGallery/PhotosCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotosCollectionViewCell.swift
+//  MyPhotoGallery
+//
+//  Created by macbook on 30.11.22.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+}
